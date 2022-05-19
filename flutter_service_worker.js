ddfdf7dfd2ec46489dfd35d006c00de5051fa64e
guchengxi1994 <528490652@qq.com>
2022-05-19 13:11:12 +0000

    Deploying to main from @ guchengxi1994/taichi_board@a2d969ebc5dcfab2f97c940186f1ccd58759e145 🚀

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,14 +3,14 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "main.dart.js": "7d1eb20862af66010a34437921134238",
+  "main.dart.js": "fc942fc222b2607e452b1d5f3429d22e",
 "canvaskit/canvaskit.wasm": "4b83d89d9fecbea8ca46f2f760c5a9ba",
 "canvaskit/profiling/canvaskit.wasm": "95e736ab31147d1b2c7b25f11d4c32cd",
 "canvaskit/profiling/canvaskit.js": "ae2949af4efc61d28a4a80fffa1db900",
 "canvaskit/canvaskit.js": "c2b4e5f3d7a3d82aed024e7249a78487",
 "flutter.js": "0816e65a103ba8ba51b174eeeeb2cb67",
-"index.html": "6eceb53b66db5a63693c9168b4a54973",
-"/": "6eceb53b66db5a63693c9168b4a54973",
+"index.html": "a682cf4e1d6e3db2ca70238145c04484",
+"/": "a682cf4e1d6e3db2ca70238145c04484",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "version.json": "1417f958c774f2de6cfec2328dfcaee5",
 "assets/AssetManifest.json": "2efbb41d7877d10aac9d091f58ccd7b9",
